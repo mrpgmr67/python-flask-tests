45ea1ec7ca5ac189e2795acd1b46b0e53090d5c9
Michael S. Pomatto <mike@pomatto.com>
2021-11-03 16:48:54 -0400

    Updated with ability to add articles

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def login():
             error = 'Username not found'
             return render_template('login.html', error=error)
         
-    return render_template('login.html')
+    return render_template('login.html') 
 
 # Check if user logged in
 def is_logged_in(f):
@@ -119,6 +119,7 @@ def is_logged_in(f):
 
 # Logout
 @app.route('/logout')
+@is_logged_in
 def logout():
     session.clear()
     flash('You are now logged out', 'success')
@@ -129,6 +130,39 @@ def logout():
 @is_logged_in
 def dashboard():
     return render_template('dashboard.html')
+
+#Article Form Class
+class ArticleForm(Form):
+    title = StringField('Title', validators=[validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', validators=[validators.Length(min=30)])
+
+# Add Article
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+        
+        # Create Cursor
+        cur = mysql.connection.cursor()
+        
+        # Execute
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)",(title, body, session['username']))
+
+        # Commit to DB
+        mysql.connection.commit()
+        
+        # Close connection
+        cur.close()
+        
+        flash('Article Created', 'success')
+        
+        return redirect(url_for('dashboard'))
+    
+    return render_template('add_article.html', form=form)
+
 
 if __name__ == '__main__':
     app.secret_key='secret123'
