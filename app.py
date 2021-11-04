aab3601024953b4307b46a6e9bdb8a5972b371b0
Michael S. Pomatto <mike@pomatto.com>
2021-11-03 21:11:17 -0400

    Completed Lesson 4 for articles

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from data import Articles
+#from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -17,7 +17,7 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
 mysql = MySQL(app)
 
-Articles = Articles()
+#Articles = Articles()
 
 @app.route('/')
 def index():
@@ -27,13 +27,36 @@ def index():
 def about():
     return render_template('about.html')    
 
+# All Articles
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles = Articles)    
+    # Create cursor
+    cur = mysql.connection.cursor()
+    
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    if result > 0:
+        return render_template('articles.html', articles=articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('articles.html', msg=msg)
+    
+    # Close connection
+    cur.close()  
 
+# Single Article
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('article.html', id=id)    
+    # Create cursor
+    cur = mysql.connection.cursor()
+    
+    # Get article
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+    article = cur.fetchone()
+    return render_template('article.html', article=article)    
+
+
 class RegisterForm(Form):
     name = StringField('Name', validators=[validators.Length(min=1, max=50)])
     username = StringField('Username', validators=[validators.Length(min=4, max=25)])
@@ -129,7 +152,20 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-    return render_template('dashboard.html')
+    # Create cursor
+    cur = mysql.connection.cursor()
+    
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    if result > 0:
+        return render_template('dashboard.html', articles=articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('dashboard.html', msg=msg)
+    
+    # Close connection
+    cur.close()  
 
 #Article Form Class
 class ArticleForm(Form):
